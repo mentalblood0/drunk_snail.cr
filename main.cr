bd99842f84a0272800d01549c8d9b06dc1c026ed
mentalblood0 <necheporenko.stepan@yandex.ru>
2025-03-11 22:53:52 +0300

    minor enhancements

--- main.cr
+++ main.cr
@@ -1,18 +1,23 @@
 class Parser
-  getter param_regex : Regex
-  getter ref_regex : Regex
-
-  protected def self.expression_regex(open : String, operator : String, close : String, optional : String) : Regex
+  protected def self.expression_regex(open : String, operator : String, close : String, optional : String)
     Regex.new("#{Regex.escape(open)} *(?P<optional>\\(#{Regex.escape(optional)}\\))?\\(#{Regex.escape(operator)}\\)(?P<name>\\w+) *#{Regex.escape(close)}")
   end
 
-  def initialize(@open = "<!--",
-                 @close = "-->",
-                 @param = "param",
-                 @ref = "ref",
-                 @optional = "optional")
+  def initialize(open = "<!--",
+                 close = "-->",
+                 param = "param",
+                 ref = "ref",
+                 optional = "optional")
     @param_regex = Parser.expression_regex(open, param, close, optional)
     @ref_regex = Parser.expression_regex(open, ref, close, optional)
+  end
+
+  def parse_refs(line)
+    line.scan @ref_regex
+  end
+
+  def parse_params(line)
+    line.scan @param_regex
   end
 end
 
@@ -44,14 +49,10 @@ class Template
 
   def initialize(text : String, parser : Parser = Parser.new)
     text.each_line do |line|
-      refs = line.scan parser.ref_regex
-      if refs.size > 1
-        raise ParseError.new "Line `#{line}` contain more then one reference expressions"
-      end
-      params = line.scan parser.param_regex
-      if refs.size > 0 && params.size > 0
-        raise ParseError.new "Line `#{line}` mixes parameters and references expressions"
-      end
+      refs = parser.parse_refs line
+      raise ParseError.new "Line `#{line}` contain more then one reference expressions" if refs.size > 1
+      params = parser.parse_params line
+      raise ParseError.new "Line `#{line}` mixes parameters and references expressions" if refs.size > 0 && params.size > 0
       if refs.size > 0
         @lines << RefLine.new refs[0]
       elsif params.size > 0
@@ -59,21 +60,18 @@ class Template
         last_param_end = 0
         params.each do |param|
           plain = line[last_param_end, param.begin - last_param_end]
-          if plain.size > 0
-            result << plain
-          end
+          result << plain if plain.size > 0
           result << Expression.new param
           last_param_end = param.end
         end
         plain = line[last_param_end, line.size]
-        if plain.size > 0
-          result << plain
-        end
+        result << plain if plain.size > 0
         @lines << result
       else
         @lines << line
       end
     end
+    puts @lines
   end
 
   def render
@@ -88,4 +86,4 @@ end
 class RenderError < Exception
 end
 
-# t = Template.new "left<!-- (param)p1 -->middle<!-- (param)p2 -->right\nplain text\nleft<!-- (ref)r -->right"
+Template.new "left<!-- (param)p1 -->middle<!-- (param)p2 -->right\nplain text\nleft<!-- (ref)r -->right"
